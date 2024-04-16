92c2e3f0aafd3615b144f1689b383d8de513f672
jessetechgeek <29627103+jessetechgeek@users.noreply.github.com>
2024-04-16 17:05:18 +0530

    added repositories in persistance project

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace HR.LeaveManagement.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
